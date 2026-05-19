12f31d37a805ad803df2dac4091cf9d881fbc839
Jerlin2465 <jerlinjack246@gmail.com>
2026-05-19 11:37:05 +0530

    FEAT:Banner_update

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -7,38 +7,60 @@ const Banner = () => {
 
   const API_URL = import.meta.env.VITE_API_URL;
 
-  const getBanners = async () => {
-    try {
-      const res = await axios.get(`${API_URL}/banner/all`);
-
-      setBanners(res.data.banners || []);
-    } catch (error) {
-      console.log(error);
-      setBanners([]);
-    }
-  };
-
   useEffect(() => {
-    getBanners();
+    axios
+      .get(`${API_URL}/banner/all`)
+      .then((res) => {
+        setBanners(res.data.banners || []);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <>
-      <Box sx={{ mt: 11 }}>
-        {banners.map((banner) => (
-          <div key={banner._id}>
+    <Box
+      sx={{
+        mt: 11,
+        overflow: "hidden",
+        width: "100%",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          width: "max-content",
+          animation: "scroll 20s linear infinite",
+          gap: 1,
+
+          "@keyframes scroll": {
+            "0%": {
+              transform: "translateX(0)",
+            },
+            "100%": {
+              transform: "translateX(-50%)",
+            },
+          },
+        }}
+      >
+        {[...banners, ...banners].map((banner, index) => (
+          <Box
+            key={index}
+            sx={{
+              minWidth: "100vw",
+              flexShrink: 0,
+            }}
+          >
             <img
               src={`${API_URL}/uploads/${banner.image}`}
               alt="banner"
               style={{
                 width: "100%",
-                height: "600px",
+                height: "650px",
               }}
             />
-          </div>
+          </Box>
         ))}
       </Box>
-    </>
+    </Box>
   );
 };
 
